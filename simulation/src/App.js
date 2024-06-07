1a87ae577b9065307b47a241c1b0d7b1c768f2d6
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-07 20:34:01 +0530

    Graph added

--- simulation/src/App.js
+++ simulation/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Simulator from './Simulator';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState();
   
   return (
     <div className="App">
       <header className="App-header">
         <div className="Input-container">
-          <input></input>
+          <input onChange={(e)=>setData(e.target.value)}></input>
         </div>
         <div className="Simulator-container">
-          <Simulator/>
+          <Simulator Data ={data}/>
         </div>
       </header>
     </div>
--- simulation/src/Simulator.js
+++ simulation/src/Simulator.js
@@ -1,5 +1,27 @@
-const Simulator=()=>{
+import CanvasJSReact from '@canvasjs/react-charts';
+import './Simulator.css';
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
+const Simulator=(data)=>{
+    const {Data} =data;
+    const options ={
+        animationEnabled: true,
+        exportEnabled: true,
+        theme: "light2",
+        title:{
+            text: "Verilog Simulator"
+        }
+    }
+    return(
+        <>
+        {options?(
+            <div className='chart-conatainer'>
+                <CanvasJSChart options={options}/>
+            </div>
+        ):null}
+
+        </>
+    )
 }
 
 export default Simulator;
